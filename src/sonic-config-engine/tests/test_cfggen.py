80a74fa937f5a16dcb1195ef442259f62a8db254
Rodny Molina <rmolina@linkedin.com>
2017-12-06 00:14:52 -0800

    Add interface-description UT and adjust logic to have 'description' field being optional
    RB=
    G=lnos-reviewers
    R=ntrianta,rjonnadu,rmolina,sfardeen,zxu
    A=

--- src/sonic-config-engine/tests/test_cfggen.py
+++ src/sonic-config-engine/tests/test_cfggen.py
@@ -133,4 +133,4 @@ class TestCfgGen(TestCase):
     def test_minigraph_ethernet_interfaces(self):
         argument = '-m "' + self.sample_graph_simple + '" -p "' + self.port_config + '" -v "PORT[\'Ethernet8\']"'
         output = self.run_script(argument)
-        self.assertEqual(output.strip(), "{'alias': 'fortyGigE0/8', 'lanes': '37,38,39,40', 'speed': '40000'}")
+        self.assertEqual(output.strip(), "{'alias': 'fortyGigE0/8', 'lanes': '37,38,39,40', 'description': 'Interface description', 'speed': '40000'}")
