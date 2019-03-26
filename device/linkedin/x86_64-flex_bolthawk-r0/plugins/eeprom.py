3232bb64ea6a1d4de0baae0711e13b683ca37367
Sangita Maity <sangitamaity0211@gmail.com>
2019-03-26 11:40:07 -0700

    [device]: add eeprom support for Open19 BOLT switch
    RB=1608544
    G=lnos-reviewers
    R=pchaudhary,pmao,rmolina,samaity,sfardeen,zxu
    A=zxu

--- device/linkedin/x86_64-flex_bolthawk-r0/plugins/eeprom.py
+++ device/linkedin/x86_64-flex_bolthawk-r0/plugins/eeprom.py
@@ -1,23 +1,86 @@
 #!/usr/bin/env python
 
-#############################################################################
-# Linkedin open19 Bolt -- TBD later
-#
-# Platform and model specific eeprom subclass, inherits from the base class,
-# and provides the followings:
-# - the eeprom format definition
-# - specific encoder/decoder if there is special need
-#############################################################################
+#################################################################################
+# Linkedin open19 Bolt eeprom plugin						#
+# Platform and model specific eeprom subclass, inherits from the base class	#
+# and provides the followings:							#
+#	- the eeprom format definition						#
+# Obtain the proper eeprom information from BMC API				#
+#################################################################################
+
+from __future__ import print_function
+from __future__ import absolute_import
 
 try:
-    from sonic_eeprom import eeprom_tlvinfo
+    import os
+    import exceptions
+    import subprocess
+    import json
+    import requests
+    import sys
+    from urlheader import UrlList
+    from syslog_helper import LogHelper
+
 except ImportError, e:
     raise ImportError (str(e) + "- required module not found")
 
+SYSLOG_IDENTIFIER = "eeprom"
 
-class board(eeprom_tlvinfo.TlvInfoDecoder):
 
-    def __init__(self, name, path, cpld_root, ro):
-        self.eeprom_path = "/sys/class/i2c-adapter/i2c-2/2-0050/eeprom"
-        super(board, self).__init__(self.eeprom_path, 0, '', True)
+# ========================== Methods for printing ==========================
 
+class board(UrlList):
+
+    def __init__(self, path, start, status, ro):
+        self.cache_name = None
+        self.cache_update_needed = False
+        self.lock_file = None
+	super(board, self).__init__()
+
+    def get_respose(self):
+
+	# get respose from url with or without authentication after verification
+	data_dict = UrlList().url_respose("eeprom")
+	if self.PLATFM_STATE in data_dict.keys():
+		platform_info = {k:v for k,v in data_dict[self.PLATFM_STATE][0].items()}
+		return platform_info
+	else:
+		LogHelper(SYSLOG_IDENTIFIER).log_error("{} key is not available".format(self.PLATFM_STATE))
+		raise KeyError("{} key is not available".format(self.PLATFM_STATE))
+
+    def read_eeprom(self):
+        self.cmd_dict = {}
+        self.cmd_dict = self.get_respose()
+        return self.cmd_dict
+
+    def decode_eeprom(self, e):
+        print("    Name                            Value    ")
+        print("----------------------------  ---------------")
+        for name in e.keys():
+            print( "%-30s  %s" % (name, e[name]))
+
+    def is_checksum_valid(self, e):
+	"""TODO: BMC needs to provide this in the rest API result,
+	 and the code here will be changed accordingly."""
+        return (True, 0)
+
+    def serial_number_str(self, e):
+	serial_key = [k for k,v in d.iteritems() if 'pcba serial number' in k.lower()]
+        if serial_key:
+		return e[serial_key][0]
+	else:
+		LogHelper(SYSLOG_IDENTIFIER).log_error("serial_key is not available in the API")
+		raise IndexError("serial_key is not available in the API")
+
+    def mgmtaddrstr(self,e):
+	if "BMC MAC" in e.keys():
+		return e["BMC MAC"]
+	else:
+		LogHelper(SYSLOG_IDENTIFIER).log_error("MAC INFO is not available")
+		raise KeyError("MAC INFO is not available")
+
+    def set_cache_name(self, name):
+        self.cache_name = name
+
+    def check_status(self):
+        return 'ok'
