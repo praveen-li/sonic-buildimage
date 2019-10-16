12f8e485c83621fa2e9549013a7a56374b9ef16a
Praveen Chaudhary <pchaudhary@linkedin.com>
2019-10-16 10:19:44 -0700

    [build_debian.sh]: Set sysctl net.netfilter.nf_conntrack_checksum to 0
    Netfilter code does not perform UDP6 checksum validation same as kernel,
    which causes kernel to drop the packet. Disable checksum validation at
    netfilter, so that only kernel performs full checksum validation.
    
    RB=1843761
    G=lnos-reviewers
    R=pchaudhary,pmao,rmolina,samaity,sfardeen,zxu
    A=

--- build_debian.sh
+++ build_debian.sh
@@ -366,6 +366,8 @@ set /files/etc/sysctl.conf/net.ipv6.conf.eth0.accept_ra 0
 
 set /files/etc/sysctl.conf/net.core.rmem_max 2097152
 set /files/etc/sysctl.conf/net.core.wmem_max 2097152
+
+set /files/etc/sysctl.conf/net.netfilter.nf_conntrack_checksum 0
 " -r $FILESYSTEM_ROOT
 
 ## docker-py is needed by Ansible docker module
