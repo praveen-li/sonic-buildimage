338d911011d8cf1d7f2d9e579a9be09a918f9834
yozhao101 <56170650+yozhao101@users.noreply.github.com>
2019-11-05 18:32:14 -0800

    [Services] Restart DHCP-Relay service upon unexpected critical process exit. (#3667) Signed-off-by: Yong Zhao <yozhao@microsoft.com>
    [Services] Restart Platform-monitor service upon unexpected critical process exit. (#3689)
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>
    
    Signed-off-by: Sangita Maity <sangitamaity0211@gmail.com>
    
        RB=2126600
        G=lnos-reviewers
        R=pchaudha,pmao,vapatil,zxu
        A=zxu

--- rules/docker-platform-monitor.mk
+++ rules/docker-platform-monitor.mk
@@ -33,3 +33,4 @@ $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/local/bin/act2_util:/usr/local/bin
 $(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /usr/lib/python2.7/dist-packages/arista:/usr/lib/python2.7/dist-packages/arista:ro
 
 $(DOCKER_PLATFORM_MONITOR)_BASE_IMAGE_FILES += sensors:/usr/bin/sensors
+$(DOCKER_PLATFORM_MONITOR)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
