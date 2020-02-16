95d87758c65d9c7821f2b558da89ed0fc448cdb4
Ping Mao <pmao@linkedin.com>
2020-02-16 13:26:40 -0800

    Add node_exporter to sonic-telemetry docker
        Signed-off-by: Ping Mao <pmao@linkedin.com>
    
           RB=1939759
           G=lnos-reviewers
           R=pchaudha,vapatil,zxu
           A=

--- dockers/docker-sonic-telemetry/start.sh
+++ dockers/docker-sonic-telemetry/start.sh
@@ -7,9 +7,6 @@ rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
 
-# don't start telemetry daemons by default
-echo "telemetry daemons are not started"
-exit 0
-
 supervisorctl start telemetry
 supervisorctl start dialout
+supervisorctl start node_exporter
