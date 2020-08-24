6b28704ac325ebbb6b3d129b55704de54161d8bd
Zhenggen Xu <zxu@linkedin.com>
2020-08-23 21:06:28 -0700

    Revert "Support snmp on FRR for LNOS"
    This reverts commit 4ed334d572816c27a41ff4bee2c10dc4f8f9d19e.
    
     Conflicts:
    	dockers/docker-fpm-frr/daemons.conf
    	src/sonic-frr/frr

--- rules/frr.mk
+++ rules/frr.mk
@@ -5,7 +5,6 @@ export FRR_VERSION
 
 FRR = frr_$(FRR_VERSION)-1~sonic.debian8+1_amd64.deb
 $(FRR)_DEPENDS += $(LIBSNMP_DEV)
-$(FRR)_RDEPENDS += $(LIBSNMP)
 $(FRR)_SRC_PATH = $(SRC_PATH)/sonic-frr
 SONIC_MAKE_DEBS += $(FRR)
 
