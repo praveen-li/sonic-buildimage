681fe8aa357c2e42f179b00c880253c22c7a6583
svc-lnos-user <svc-lnos-user@linkedin.com>
2020-09-02 22:23:45 -0700

    Release Image commit: Tag lnos_v2.2.2

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -10,7 +10,7 @@
 #       then add the aforementioned runtime dependencies here.
 #
 
-SONIC_UTILS = python-sonic-utilities_2.2.1-1_all.deb
+SONIC_UTILS = python-sonic-utilities_2.2.2-1_all.deb
 $(SONIC_UTILS)_SRC_PATH = $(SRC_PATH)/sonic-utilities
 $(SONIC_UTILS)_WHEEL_DEPENDS = $(SONIC_CONFIG_ENGINE)
 SONIC_PYTHON_STDEB_DEBS += $(SONIC_UTILS)
