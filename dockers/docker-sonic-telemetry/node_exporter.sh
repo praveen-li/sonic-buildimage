c333e638bde5920eaa93f12597a9e1b1e60de0a8
Ping Mao <pmao@linkedin.com>
2020-12-09 15:41:09 -0800

    SONiC Telemetry - Enable Node Exporter Textfile Collector
       Address review feedback
    
           Signed-off-by: Ping Mao <pmao@linkedin.com>
           RB=2396917
           G=lnos-reviewers
           R=pchaudhary,pmao,vapatil,samaity,zxu
           A=

--- dockers/docker-sonic-telemetry/node_exporter.sh
+++ dockers/docker-sonic-telemetry/node_exporter.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# node_exporter testfile_collector dir
+NODE_EXPORTER_TEXT_COLL_DIR="/var/lib/node_exporter/textfile_collector"
+
 # create node-exporter text_collector directory
-mkdir -p /var/lib/node_exporter/textfile_collector
+mkdir -p $NODE_EXPORTER_TEXT_COLL_DIR
 
 # Start with default config
-NODE_EXPORTER_OPTS="--collector.textfile.directory /var/lib/node_exporter/textfile_collector"
+NODE_EXPORTER_OPTS="--collector.textfile.directory $NODE_EXPORTER_TEXT_COLL_DIR"
 exec /usr/sbin/node_exporter $NODE_EXPORTER_OPTS
