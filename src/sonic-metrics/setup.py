020a71286a83b6a2e29c0ca8f110bc2635c9da62
Sangita Maity <samaity@linkedin.com>
2021-02-11 18:24:32 +0000

    [sonic-metrics] Introduce ProcessInfoUpdateTask in a new SONiC metrics daemon to store Critical Process Info into STATE DB
    Signed-off-by: Sangita Maity <samaity@linkedin.com>
    
    RB=2465220
    G=lnos-reviewers
    R=pchaudhary,pmao,vapatil,samaity,zxu
    A=pmao

--- src/sonic-metrics/setup.py
+++ src/sonic-metrics/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 dependencies = [
     'redis==2.10.6',
+    'psutil'
 ]
 
 high_performance_deps = [
@@ -11,7 +12,9 @@ high_performance_deps = [
 setup(
     name='sonic_metrics',
     version='1.0',
-    packages=['metrics'],
+    packages=[
+        'metrics'
+    ],
     scripts=[
         'scripts/sonic_metricsd',
     ],
@@ -21,6 +24,11 @@ setup(
     maintainer="lnos-coders",
     maintainer_email='lnos-coders@linkedin.com',
     description='SONiC metrics python scripts',
+    install_requires = dependencies,
+    package_data = {
+        'metrics': ['data/critical_process_file.json']
+    },
+    include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: Linux',
