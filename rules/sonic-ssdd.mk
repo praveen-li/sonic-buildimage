f838998755f20c8e616d1d374c2561d2000ee39f
Ping Mao <pmao@linkedin.com>
2021-03-10 18:13:29 -0800

    Remove unused file Change the ssd python wheel package version
        Signed-off-by: Ping Mao <pmao@linkedin.com>
    
        RB=2504716
        G=lnos-reviewers
        R=pchaudha,pmao,samaity,zxu
        A=

--- rules/sonic-ssdd.mk
+++ rules/sonic-ssdd.mk
@@ -1,5 +1,5 @@
 # sonic-ssdd (SONiC SSD daemon) Debian package
 
-SONIC_SSDD = python-sonic-ssdd_1.1-1_all.deb
+SONIC_SSDD = python-sonic-ssdd_1.0-1_all.deb
 $(SONIC_SSDD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-ssdd
 SONIC_PYTHON_STDEB_DEBS += $(SONIC_SSDD)
