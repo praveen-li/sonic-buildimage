4b87936c1ff1e05be6348cbb8ce9eacfe6b83816
Zhenggen Xu <zxu@linkedin.com>
2021-04-20 15:47:37 -0700

    Revert "[sonic-metrics] Introduce ProcessInfoUpdateTask in a new SONiC metrics daemon to store Critical Process Info into STATE DB"
    This reverts commit e855a986ec2401fa2a3e67877433043eeac9ed9a.
    
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>
    
    RB=2555868
    G=lnos-reviewers
    R=pchaudha,pmao,samaity,zxu
    A=

--- src/sonic-metrics/setup.py
+++ src/sonic-metrics/setup.py
@@ -11,9 +11,7 @@ high_performance_deps = [
 setup(
     name='sonic_metrics',
     version='1.0',
-    packages=[
-        'metrics'
-    ],
+    packages=['metrics'],
     scripts=[
         'scripts/sonic_metricsd',
     ],
@@ -23,13 +21,6 @@ setup(
     maintainer="lnos-coders",
     maintainer_email='lnos-coders@linkedin.com',
     description='SONiC metrics python scripts',
-    install_requires = [
-        'psutil'
-    ],
-    package_data = {
-        'metrics': ['data/critical_process_file.json']
-    },
-    include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: Linux',
