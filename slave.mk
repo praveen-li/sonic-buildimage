97137bb82933767f933cb43f892950dca876ef1b
Praveen Chaudhary <pchaudhary@linkedin.com>
2021-08-09 17:51:23 -0400

    Addresed below review comments.
    – remove unnecessary changes about submodules from .gitmodule.
    – sysctl moved to files/image_config/sysctl/sysctl-net.conf.
    – remove package to support Dynamic Port Breakout from build_debian.sh
    – remove exports for STRETCH_DEBS_PATH and for PYTHON_WHEELS_PATH from
    slave.mk.
    – remove dependency PKGs for sonic_yang_mgmt build from
    sonic-slave-stretch/Dockerfile.j2.
    – remove src/sonic-device-data/tests/platformJson_checker.
    – enable RA by default for all localhost types.
    
    Signed-off-by: Praveen Chaudhary <pchaudhary@linkedin.com>

--- slave.mk
+++ slave.mk
@@ -61,8 +61,6 @@ export CONFIGURED_ARCH
 export PYTHON_WHEELS_PATH
 export IMAGE_DISTRO
 export IMAGE_DISTRO_DEBS_PATH
-export STRETCH_DEBS_PATH
-export PYTHON_WHEELS_PATH
 export MULTIARCH_QEMU_ENVIRON
 
 ###############################################################################
