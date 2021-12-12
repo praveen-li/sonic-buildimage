b95740cdfc3f36292ea06f6d5a2ea5007ed0b94a
ankgoyal <ankgoyal@cisco.com>
2021-12-11 21:08:00 -0800

    changes to build sonic_platform as *.deb instead of *.whl

--- platform/broadcom/sonic-platform-modules-cisco/sonic-platform/setup.py
+++ platform/broadcom/sonic-platform-modules-cisco/sonic-platform/setup.py
@@ -19,5 +19,17 @@ setup(
        'sonic-platform-common',
        'sonic-py-common'
    ],
+   classifiers=[
+       'Development Status :: 3 - Alpha',
+       'Environment :: Plugins',
+       'Intended Audience :: Developers',
+       'Intended Audience :: Information Technology',
+       'Intended Audience :: System Administrators',
+       'License :: OSI Approved :: Apache Software License',
+       'Natural Language :: English',
+       'Operating System :: POSIX :: Linux',
+       'Programming Language :: Python :: 3.7',
+       'Topic :: Utilities',
+    ],
    keywords='sonic SONiC platform PLATFORM',
 )
