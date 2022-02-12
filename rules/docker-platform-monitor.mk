86f8e1f192810f81dc0060acdfcc983b4b06e5e3
Ping Mao <pmao@linkedin.com>
2022-02-12 04:47:18 -0500

    thermalctld is not running
                Signed-off-by: Ping Mao <pmao@linkedin.com>
    
                RB=3127402
                G=lnos-reviewers
                R=pchaudha,pmao,samaity,stexu,zxu
                A=

--- rules/docker-platform-monitor.mk
+++ rules/docker-platform-monitor.mk
@@ -64,8 +64,6 @@ $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /var/run/platform_cache:/var/run/platfo
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/share/sonic/device/pddf:/usr/share/sonic/device/pddf:ro
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/local/bin/pfm_util:/usr/local/bin/pfm_util:ro
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/local/bin/act2_util:/usr/local/bin/act2_util:ro
-$(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/lib/python2.7/dist-packages/sonic_platform:/usr/lib/python2.7/dist-packages/sonic_platform:ro
-$(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /usr/lib/python3/dist-packages/sonic_platform:/usr/lib/python3/dist-packages/sonic_platform:ro
 
 # Mount Arista python library on Aboot images to be used by plugins
 $(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /usr/lib/libsfp-eeprom.so:/usr/lib/libsfp-eeprom.so:ro
