f89e42c98721ed3406d23e9ab8173045165d130f
Zhenggen Xu <zxu@linkedin.com>
2022-03-14 03:26:10 -0400

    [Seastone/3232C] RDMA/Qos support
    - Introduce reusable template and add custom cable lenght without device_neighbor_metadata
    - Seastone HWSKUs:
      ASW: Seastone-DX010-25-50, Seastone-DX010-25-100
      CSW: Seastone-DX010-50, Seastone-DX010-50-40
           Seastone-DX010-50-50-40, Seastone-DX010-40-50-100
           Seastone-DX010
    - 3232C HWSKUs:
      ASW: Cisco-3232C-25-50, Cisco-3232C-25-100
      CSW: Cisco-3232C-50, Cisco-3232C-32x50-16x100
           Cisco-3232C-48x50-8x100, Cisco-3232C-16x100-16x50-8x100
           Cisco-3232C-100
    - Fix the PG lookup file with right value
    - Linkedin cable lengths
    - LinkedIn MMU init profile: LI-TH-Tier0, LI-TH-Tier1 with new SAI binary
    - ECN DP value to be 10%
    - Update TC to PG mapping
    - Fix the sonic-cfggen j2 unit tests
    
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>
    
    RB=3247275
    G=lnos-reviewers
    R=pchaudha,pmao,samaity,stexu,zxu
    A=

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.3_amd64.deb
-$(BRCM_SAI)_URL = "http://172.21.47.11/lnos/sonic/SAI-package/libsaibcm_4.3.5.3_amd64.deb"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.3-LI2_amd64.deb
+$(BRCM_SAI)_URL = "http://172.21.47.11/lnos/sonic/SAI-package/libsaibcm_4.3.5.3-LI2_amd64.deb"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-LI2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "http://172.21.47.11/lnos/sonic/SAI-package/libsaibcm-dev_4.3.5.3_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "http://172.21.47.11/lnos/sonic/SAI-package/libsaibcm-dev_4.3.5.3-LI2_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
