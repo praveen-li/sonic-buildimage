d5b775bc8c100956ddfc69212d1b461ef3f62fde
svc-lnos-user <svc-lnos-user@linkedin.com>
2022-05-04 02:24:42 -0400

    Release Image commit: Tag lnos_v3.2.0

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -1,6 +1,6 @@
 # sonic utilities package
 
-SONIC_UTILITIES_PY3 = sonic_utilities-3.1.0-py3-none-any.whl
+SONIC_UTILITIES_PY3 = sonic_utilities-3.2.0-py3-none-any.whl
 $(SONIC_UTILITIES_PY3)_SRC_PATH = $(SRC_PATH)/sonic-utilities
 $(SONIC_UTILITIES_PY3)_PYTHON_VERSION = 3
 $(SONIC_UTILITIES_PY3)_DEPENDS += $(SONIC_PY_COMMON_PY3) \
